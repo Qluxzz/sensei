dbfb4efaf4211cc159f4385948180c57b25b2059
Qluxzz <andre.eric.andersson@gmail.com>
2023-05-23 09:38:20 +0200

    Get file from ftp direct

--- convert_xml.py
+++ convert_xml.py
@@ -1,31 +1,42 @@
-import xml.etree.ElementTree as ET
 import csv
+import gzip
+import urllib.request
+import xml.etree.ElementTree as ET
 
 
 def main():
     # https://www.edrdg.org/wiki/index.php/JMdict-EDICT_Dictionary_Project
-    tree = ET.parse("JMdict_e")
 
-    with open("words.csv", mode="w+") as csvfile:
-        writer = csv.writer(csvfile, delimiter=',', quotechar="'")
+    with urllib.request.urlopen("http://ftp.edrdg.org/pub/Nihongo/JMdict_e.gz") as gziped:
+        with gzip.open(gziped) as file:
+            with open("words.csv", mode="w+") as csv_file:
+                writer = csv.writer(
+                    csv_file,
+                    delimiter=',',
+                    quotechar='\'',
+                    escapechar='\\',
+                    quoting=csv.QUOTE_ALL
+                )
 
-        for child in tree.iter("entry"):
-            k_ele = child.find("k_ele")
+                tree = ET.parse(file)
 
-            if k_ele is None:
-                continue
+                for child in tree.iter("entry"):
+                    k_ele = child.find("k_ele")
 
-            if k_ele.find("keb") is None:
-                continue
+                    if k_ele is None:
+                        continue
 
-            if should_skip(child):
-                continue
+                    if k_ele.find("keb") is None:
+                        continue
 
-            word = k_ele.find("keb").text
-            normalized = child.find("r_ele").find("reb").text
-            glossary = [x.text for x in child.iter("gloss")]
+                    if should_skip(child):
+                        continue
 
-            writer.writerow([word, normalized, "|".join(glossary)])
+                    word = k_ele.find("keb").text
+                    normalized = child.find("r_ele").find("reb").text
+                    glossary = [x.text for x in child.iter("gloss")]
+
+                    writer.writerow([word, normalized, "|".join(glossary)])
 
 
 ignored_fields = ["field", "misc"]
