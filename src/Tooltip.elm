f1203aa27abce99ec10503a8cd1c16242dccc5b3
Qluxzz <andre.eric.andersson@gmail.com>
2023-05-29 14:39:15 +0200

    Remove tab index from tooltip

--- src/Tooltip.elm
+++ src/Tooltip.elm
@@ -8,4 +8,9 @@ import Html.Attributes
 -}
 withTooltip : String -> String -> Html msg
 withTooltip text_ tooltip =
-    Html.button [ Html.Attributes.type_ "button", Html.Attributes.attribute "data-tooltip" tooltip ] [ text text_ ]
+    Html.button
+        [ Html.Attributes.type_ "button"
+        , Html.Attributes.attribute "data-tooltip" tooltip
+        , Html.Attributes.tabindex -1
+        ]
+        [ text text_ ]
