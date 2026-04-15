2ff6725726b157e53b59138faf63184c46cb518c
Qluxzz <andre.eric.andersson@gmail.com>
2026-04-15 23:01:38 +0200

    Fix service worker with Vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
 import { defineConfig } from "vite"
 import elm from "vite-plugin-elm"
+import { VitePWA } from "vite-plugin-pwa"
 
 export default defineConfig(({ mode }) => ({
   plugins: [
     elm({
       debug: mode !== "production",
       optimize: mode === "production",
+    }),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "inline",
+      manifest: {
+        short_name: "Elm Template",
+        name: "Elm Template",
+        description: "This is the description of the Elm Template app",
+        icons: [],
+        start_url: "/",
+        display: "browser",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+      },
+      workbox: {
+        globPatterns: ["**/*.{js,css,html}"],
+      },
     }),
   ],
   build: {
